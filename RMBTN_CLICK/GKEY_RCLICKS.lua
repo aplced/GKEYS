34a67d3d7b39dd050a465d469574787fe93cdd8e
Atanas Andreev <apleced@gmail.com>
2012-10-26 13:37:31 +0300

    Convert tabs to spaces

--- RMBTN_CLICK/GKEY_RCLICKS.lua
+++ RMBTN_CLICK/GKEY_RCLICKS.lua
@@ -7,78 +7,78 @@ gInitT = {{}, {}, {}}
 gFuncT = {{}, {}, {}}
 
 function CreateAndSaveGKeyData(mode, arg)
-	local destX,destY = GetMousePosition()
-	gInitT[mode][arg] = {}
-	gInitT[mode][arg].x = destX
-	gInitT[mode][arg].y = destY
-	
-	WriteBindings()
+    local destX,destY = GetMousePosition()
+    gInitT[mode][arg] = {}
+    gInitT[mode][arg].x = destX
+    gInitT[mode][arg].y = destY
 
-	return destX, destX
+    WriteBindings()
+
+    return destX, destX
 end
 
 function CreateGKeyMouseClick(destX, destY)
-	-- local destX,destY = GetMousePosition()
-	-- OutputLogMessage("Create click function for(%d,%d)\n", destX, destY)
-	return function ()
-			origX,origY = GetMousePosition()
-			-- OutputLogMessage("Origin coordinates(%d,%d)\n", origX, origY)
-			-- OutputLogMessage("Destination coordinates(%d,%d)\n", destX, destY)
-			MoveMouseTo(destX,destY)
-			Sleep(10)
-			PressAndReleaseMouseButton(3)
-			Sleep(10)
-			MoveMouseTo(origX,origY)
-			end
+    -- local destX,destY = GetMousePosition()
+    -- OutputLogMessage("Create click function for(%d,%d)\n", destX, destY)
+    return function ()
+            origX,origY = GetMousePosition()
+            -- OutputLogMessage("Origin coordinates(%d,%d)\n", origX, origY)
+            -- OutputLogMessage("Destination coordinates(%d,%d)\n", destX, destY)
+            MoveMouseTo(destX,destY)
+            Sleep(10)
+            PressAndReleaseMouseButton(3)
+            Sleep(10)
+            MoveMouseTo(origX,origY)
+            end
 end
 
 function BindGKey(mode, arg)
-	gFuncT[mode][arg] = CreateGKeyMouseClick(CreateAndSaveGKeyData(mode, arg))
+    gFuncT[mode][arg] = CreateGKeyMouseClick(CreateAndSaveGKeyData(mode, arg))
 end
 
 function InitGKeys()
-	for mode,mv in pairs(gInitT) do
-		for key, kv in pairs(mv) do
-			gFuncT[mode][key] = CreateGKeyMouseClick(kv.x, kv.y)
-		end
-	end
+    for mode,mv in pairs(gInitT) do
+        for key, kv in pairs(mv) do
+            gFuncT[mode][key] = CreateGKeyMouseClick(kv.x, kv.y)
+        end
+    end
 end
 
 function WriteBindings()
-	ClearLog()
-	OutputLogMessage(serialize(gInitT))
+    ClearLog()
+    OutputLogMessage(serialize(gInitT))
 end
 
 function serialize (o)
-	tSer = ""
-	if type(o) == "table" then
-		tSer = tSer.."{"
-		for k,v in pairs(o) do
-			if type(k) == "number" then
-				tSer = tSer.."["..k.."]="
-			else
-				tSer = tSer..k.."="
-			end
-			tSer = tSer..serialize(v)
-			tSer = tSer..","
-		end
-		tSer = tSer.."}"
-	else
-		tSer = tSer..o
-	end
-	return tSer
+    tSer = ""
+    if type(o) == "table" then
+        tSer = tSer.."{"
+        for k,v in pairs(o) do
+            if type(k) == "number" then
+                tSer = tSer.."["..k.."]="
+            else
+                tSer = tSer..k.."="
+            end
+            tSer = tSer..serialize(v)
+            tSer = tSer..","
+        end
+        tSer = tSer.."}"
+    else
+        tSer = tSer..o
+    end
+    return tSer
 end
 
 function OnEvent(event, arg)
-	if event == "PROFILE_ACTIVATED" then
-		InitGKeys()
+    if event == "PROFILE_ACTIVATED" then
+        InitGKeys()
     elseif event == "G_PRESSED" then
-		mode = GetMKeyState("kb")
-		if(IsModifierPressed("ctrl")) then
-			BindGKey(mode, arg)
-		elseif (gFuncT[mode][arg] ~= nil) then
-			gFuncT[mode][arg]()	
-		end
-	end
-	
+        mode = GetMKeyState("kb")
+        if(IsModifierPressed("ctrl")) then
+            BindGKey(mode, arg)
+        elseif (gFuncT[mode][arg] ~= nil) then
+            gFuncT[mode][arg]()    
+        end
+    end
+    
 end
